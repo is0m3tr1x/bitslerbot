848789362f0a2b39b75e283b02912ed6298a4488
is0m3tr1x <30616394+is0m3tr1x@users.noreply.github.com>
2017-08-11 11:15:49 +1200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,6 @@
-## is0m3tr1x Bitsler Bot
+#!/bin/bash
+
+## is0m3tr1x Bitsler Bot Configuration
 ## contact is0m3tr1x.2017@gmail.com
 ## repository https://github.com/is0m3tr1x/bitslerbot
 ## donate 1MN5MBKc7CvkXqsYpFrNmzxS6ZMCE6w4AG
@@ -6,9 +8,11 @@
 ## set bitsler username
 user="is0lvlEtr1x"
 
-token="ea45fcbed13b7c41a653e0f8bccf78311a3a25053c75919cf29529035d5424174d1e663fa8994d78925b28039ab2f0933955d388858d019736c527eead92183d"
-
 ## set bitsler auth token
+token="ea45fcbed13b7c41a653e0f8bccf78311a3a25053c75919cf29529035d5424174d1e663fa89d4d78925b28039ab2f0933955d388858d019736c527eead92183d"
+
+## default settings file
+settings="./strategies/default.conf"
 
 ##----------------------------##
 ## DONT TOUCH BELOW THIS LINE ##
@@ -29,8 +33,8 @@ gt='%3E'
 lt='%3C'
 lines=13
 
-## curl arguments
-timeout="--connect-timeout 3 --max-time 4 --retry 3 --retry-delay 0 --retry-max-time 3"
+## curl arguments timeout
+timeout="--connect-timeout 10 --max-time 10 --retry 5 --retry-delay 10 --retry-max-time 10"
 
 ## web response files
 resp1="./web/resp1"
@@ -63,6 +67,3 @@ cafe="\033[0;33m"
 fiuscha="\033[0;35m"
 blue="\033[1;34m"
 transparent="\e[0m"
-
-## settings file
-settings="./settings.sh"
