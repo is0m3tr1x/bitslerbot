8cb06764fa9aa0bf023e65f17862427e68de51c8
is0m3tr1x <30616394+is0m3tr1x@users.noreply.github.com>
2017-08-11 10:43:23 +1200

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ## max potential loss calculator
 ## progressive compounded wagered calculations
 ## payout @ nth iterations for preceeding wagered amount
@@ -14,29 +16,29 @@ iter="$3"
 base="$4"
 bal="$5"
 bet=1
+wagered=0
 
-if [ ! $@ ]; then
+if [ ! "$@" ]; then
 echo "Usage......"
 echo '$1 = odds'
-echo '$2 = multiplier'
+echo '$2 = percentage increase'
 echo '$3 = amount of consecutive losses to factor'
 echo '$4 = the initial bet amount to calculate from'
-echo '$5 = balance to stop on'
+echo '$5 = inital base bet to project from'
 echo 'example ./project.sh 2 205 16 0.0000100'
 exit 0
 else
 echo "is0lvlEtr1x..."
-echo "INiTiAL BET = $base"
-echo "WAGERED AMOUNT ~ PAYOUT ~ PROFIT"
-#while [ "$bal" -gt "$base" ]; do
-until [ $iter = "0" ]; do
+echo -n "INiTiAL BET is $base"
+echo "BET AMOUNT ~ TOTAL WAGERED ~ PAYOUT ~ PROFIT"
+until [ "$iter" = "0" ]; do
 base=$(echo "scale=10; $base + ($base * ($mult / 100))" | bc)
+wagered=$(echo "scale=10; $base + $wagered" | bc)
 iter=$(echo "$iter - 1" | bc)
 bet=$(echo "$bet + 1" | bc)
 return=$(echo "scale=10; $base * $odds" | bc)
 profit=$(echo "scale=10; $return - $base" | bc)
-echo "$bet $base -> $return -> $profit"
+echo "$bet $base -> $wagered -> $return -> $profit"
 done
-#done
-echo "TOTAL BALANCE REQUIRED FOR $bet CONSECUTIVE LOSSES @ ODDS=$odds LOSS MULTIPLIER=$mult BALANCE:$base"
+echo "TOTAL BALANCE REQUIRED FOR $bet CONSECUTIVE LOSSES @ ODDS=$odds LOSS MULTIPLIER=$mult BALANCE:$wagered"
 fi
